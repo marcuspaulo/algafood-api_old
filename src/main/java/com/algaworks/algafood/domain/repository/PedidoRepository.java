de8600a3fc7d0d3bfa4b31f204cbf0f9184936c2
Marcus Paulo <mpaulobr@gmail.com>
2020-06-08 17:55:03 -0300

    Implementação da consulta de Estatísticas diárias CriteriaBuilder

--- src/main/java/com/algaworks/algafood/domain/repository/PedidoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/PedidoRepository.java
@@ -3,6 +3,8 @@ package com.algaworks.algafood.domain.repository;
 import java.util.List;
 import java.util.Optional;
 
+import com.algaworks.algafood.domain.filter.VendasDiariasFilter;
+import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -17,5 +19,4 @@ public interface PedidoRepository extends CustomJpaRepository<Pedido, Long>,
 
 	@Query("from Pedido p join fetch p.cliente join fetch p.restaurante r join fetch r.cozinha")
 	List<Pedido> findAll();
-	
 }
--- src/main/java/com/algaworks/algafood/infrastructure/repository/spec/PedidoSpecs.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/spec/PedidoSpecs.java
@@ -7,7 +7,7 @@ import javax.persistence.criteria.Predicate;
 import org.springframework.data.jpa.domain.Specification;
 
 import com.algaworks.algafood.domain.model.Pedido;
-import com.algaworks.algafood.domain.repository.filter.PedidoFilter;
+import com.algaworks.algafood.domain.filter.PedidoFilter;
 
 public class PedidoSpecs {
 
